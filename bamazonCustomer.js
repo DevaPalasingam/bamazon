80c4635fb2d9dcce983a0b825b0084bf10c07f78
Deva <deva.palasingam@gmail.com>
2017-11-05 12:25:58 -0600

    finished printing table, need to work on prompting user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-var cliTable = require("cli-table");
+var Table = require('cli-table');
+var colors = require("colors");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,7 +18,31 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-
-
+  start();
 
 });
+
+// start() - displays what's in stock and prompts user to pick an id and amount
+function start() {
+	
+	connection.query("SELECT * FROM products", function(err, res){
+		
+		var table = new Table({
+    		head: ["ID", "Product", "Department", "Price", "Amount in Stock"]
+		});
+
+		for (var i = 0; i < res.length; i++) {
+      	
+			table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+
+    	}
+    	// closes for-loop
+
+		// prints table
+		console.log(table.toString());
+
+  	});	
+  	// closes connection.query
+
+}
+// start() =====================================================
